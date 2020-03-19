6a3e4ca7639d3e2a11bd661d7601d65ccb0f4af9
vlad <vlad.bond.1998@mail.ru>
2020-03-19 09:02:16 +0300

    Update

--- task_two.py
+++ task_two.py
@@ -29,6 +29,7 @@ file_three = pd.read_csv('file_3.csv')
 print(file_three.head())
 
 cost, demand = [], []
+
 # The difference between the initial price and the current price, and the difference between the initial price and the
 # current.
 cost_er, income_er = [], []
@@ -44,49 +45,63 @@ start_average = average(cost)
 start_income = income(cost, demand)
 print(start_average, start_income)
 
-# for j in range(1000000000):
-#     rnd_mass = random.sample(range(8), random.randint(0, 8))
-    # print(rnd_mass)
-
+# The influence of each on the change in average price and total profit.
 for i in range(9):
     # Algorithm from the condition.
     cost[i] = cost[i] * 1.01
     lose = demand[i] * 0.1
     demand[i] = demand[i] - lose
     demand[i+1] = demand[i+1] + lose
-    # print(i, average(cost), income(cost, demand))
     cost_er.append(average(cost) - start_average)
     income_er.append(start_income - income(cost, demand))
-    print(average(cost), income(cost, demand))
+    print(i, round(average(cost),3), round(income(cost, demand),4))
     cost = copy.deepcopy(start_cost)
     demand = copy.deepcopy(start_demand)
-# print(cost)
-# print(demand)
-
-print(cost_er, income_er)
-# find_min(cost_er, 1, False)
-# find_min(income_er, 1, True)
-# print(cost_er, income_er)
-
-    # if(average(cost) > start_average and income(cost, demand) >= start_income):
-    #     print(rnd_mass, average(cost), income(cost, demand))
-
-    # cost = copy.deepcopy(start_cost)
-    # demand = copy.deepcopy(start_demand)
-
-
-
 print(cost)
 print(demand)
+print("Cost error", cost_er)
+print("Income error", income_er)
+# Find some min and max elements in array.
+# find_min(cost_er, 4, False)
+# find_min(income_er, 4, True)
+# print(cost_er, income_er)
 
-#   39.4   5770.5
-# 0 39.519 5748.1
-# 1 39.498 5766.246
-# 2 39.475 5743.549
-# 3 39.449 5701.717
-# 4 39.426 5764.17
-# 5 39.411 5770.199
-# 6 39.407 5769.541
-# 7 39.405 5769.8625
-# [120.0, 98.0, 75.0, 49.0, 26.0, 11.0, 7.0, 5.0, 2.0, 1.0]
-# [20.0, 3.0, 14.0, 37.0, 5.0, 1.0, 7.0, 2.5, 0.5, 10.0]
+# The effect of each price increase on the change in average price and total profit.
+for i in range(9):
+    # Algorithm from the condition.
+    cost[i] = cost[i] * 1.01
+    lose = demand[i] * 0.1
+    demand[i] = demand[i] - lose
+    demand[i+1] = demand[i+1] + lose
+    cost_er.append(average(cost) - start_average)
+    income_er.append(start_income - income(cost, demand))
+print(round(average(cost),3), round(income(cost, demand),4))
+print("Price and profit after each change")
+print(cost)
+print(demand)
+cost = copy.deepcopy(start_cost)
+demand = copy.deepcopy(start_demand)
+
+# Generates a random array and checks for changes in average price and profit.
+for j in range(1000000):
+    rnd_mass = random.sample(range(8), random.randint(0, 8))
+
+    for i in rnd_mass:
+        # Algorithm from the condition.
+        cost[i] = cost[i] * 1.01
+        lose = demand[i] * 0.1
+        demand[i] = demand[i] - lose
+        demand[i+1] = demand[i+1] + lose
+        cost_er.append(average(cost) - start_average)
+        income_er.append(start_income - income(cost, demand))
+    if(average(cost) > start_average) and (income(cost, demand) >= start_income - 1):
+        print(rnd_mass, average(cost), income(cost, demand))
+        print(cost)
+        print(demand)
+
+    cost = copy.deepcopy(start_cost)
+    demand = copy.deepcopy(start_demand)
+
+# As a result, the best average price – profit ratio is [5], [6], [7], [5, 7]. At the same time, the profit is still less than the initial one.
+
+
